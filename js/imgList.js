f7df5251294e11fceb1691571b72c067fb65d251
Nacmikea <81470587+Nacmikea@users.noreply.github.com>
2021-05-21 10:58:57 +0800

    IU的小页

---
+++ js/imgList.js
@@ -0,0 +1,173 @@
+function get_img_IU1b1_1()
+{
+	return [
+		"img/IU1b1_1/001.jpg",
+		"img/IU1b1_1/002.jpg",
+		"img/IU1b1_1/003.jpg",
+		"img/IU1b1_1/004.jpg",
+		"img/IU1b1_1/005.jpg",
+		"img/IU1b1_1/006.jpg",
+		"img/IU1b1_1/007.jpg",
+		"img/IU1b1_1/008.jpg",
+		"img/IU1b1_1/009.jpg",
+
+	];
+}
+
+function get_img_IU1b1_2()
+{
+	return [
+		"img/IU1b1_2/001.jpg",
+		"img/IU1b1_2/002.jpg",
+		"img/IU1b1_2/003.jpg",
+		"img/IU1b1_2/004.jpg",
+		"img/IU1b1_2/005.jpg",
+		"img/IU1b1_2/006.jpg",
+		"img/IU1b1_2/007.jpg",
+		"img/IU1b1_2/008.jpg",
+		"img/IU1b1_2/009.jpg",
+
+	];
+}
+
+function get_img_IU1b1_3()
+{
+	return [
+		"img/IU1b1_3/001.jpg",
+		"img/IU1b1_3/002.jpg",
+		"img/IU1b1_3/003.jpg",
+		"img/IU1b1_3/004.jpg",
+		"img/IU1b1_3/005.jpg",
+		"img/IU1b1_3/006.jpg",
+		"img/IU1b1_3/007.jpg",
+		"img/IU1b1_3/008.jpg",
+		"img/IU1b1_3/009.jpg",
+
+	];
+}
+
+function get_img_IU2b3_1()
+{
+	return [
+		"img/IU2b3_1/001.jpg",
+		"img/IU2b3_1/002.jpg",
+		"img/IU2b3_1/003.jpg",
+		"img/IU2b3_1/004.jpg",
+		"img/IU2b3_1/005.jpg",
+		"img/IU2b3_1/006.jpg",
+		"img/IU2b3_1/007.jpg",
+		"img/IU2b3_1/008.jpg",
+		"img/IU2b3_1/009.jpg",
+		"img/IU2b3_1/010.jpg",
+		"img/IU2b3_1/011.jpg",
+		"img/IU2b3_1/012.jpg",
+		"img/IU2b3_1/013.jpg",
+		"img/IU2b3_1/014.jpg",
+		"img/IU2b3_1/015.jpg",
+		"img/IU2b3_1/016.jpg",
+	];
+}
+
+
+function get_img_IU3b2_1()
+{
+	return [
+		"img/IU3b2_1/001.jpg",
+		"img/IU3b2_1/002.jpg",
+		"img/IU3b2_1/003.jpg",
+		"img/IU3b2_1/004.jpg",
+		"img/IU3b2_1/005.jpg",
+		"img/IU3b2_1/006.jpg",
+		"img/IU3b2_1/007.jpg",
+		"img/IU3b2_1/008.jpg",
+		"img/IU3b2_1/009.jpg",
+		"img/IU3b2_1/010.jpg",
+		"img/IU3b2_1/011.jpg",
+		"img/IU3b2_1/012.jpg",
+	];
+}
+
+function get_img_IU3b4_1()
+{
+	return [
+		"img/IU3b4_1/001.jpg",
+		"img/IU3b4_1/002.jpg",
+		"img/IU3b4_1/003.jpg",
+		"img/IU3b4_1/004.jpg",
+		"img/IU3b4_1/005.jpg",
+		"img/IU3b4_1/006.jpg",
+		"img/IU3b4_1/007.jpg",
+		"img/IU3b4_1/008.jpg",
+		"img/IU3b4_1/009.jpg",
+		"img/IU3b4_1/010.jpg",
+		"img/IU3b4_1/011.jpg",
+		"img/IU3b4_1/012.jpg",	
+		"img/IU3b4_1/013.jpg",
+		"img/IU3b4_1/014.jpg",
+	];
+}
+
+function get_img_IU4b3_1()
+{
+	return [
+		"img/IU4b3_1/001.jpg",
+		"img/IU4b3_1/002.jpg",
+		"img/IU4b3_1/003.jpg",
+		"img/IU4b3_1/004.jpg",
+		"img/IU4b3_1/005.jpg",
+		"img/IU4b3_1/006.jpg",
+		"img/IU4b3_1/007.jpg",
+		"img/IU4b3_1/008.jpg",
+		"img/IU4b3_1/009.jpg",
+		"img/IU4b3_1/010.jpg",
+
+	];
+}
+
+function get_img_IU4b3_2()
+{
+	return [
+		"img/IU4b3_2/001.jpg",
+		"img/IU4b3_2/002.jpg",
+		"img/IU4b3_2/003.jpg",
+		"img/IU4b3_2/004.jpg",
+		"img/IU4b3_2/005.jpg",
+		"img/IU4b3_2/006.jpg",
+		"img/IU4b3_2/007.jpg",
+		"img/IU4b3_2/008.jpg",
+		"img/IU4b3_2/009.jpg",
+
+	];
+}
+
+function get_img_IU9b16_1()
+{
+	return [
+		"img/IU9b16_1/001.jpg",
+		"img/IU9b16_1/002.jpg",
+		"img/IU9b16_1/003.jpg",
+		"img/IU9b16_1/004.jpg",
+		"img/IU9b16_1/005.jpg",
+		"img/IU9b16_1/006.jpg",
+		"img/IU9b16_1/007.jpg",
+		"img/IU9b16_1/008.jpg",
+		"img/IU9b16_1/009.jpg",
+		"img/IU9b16_1/010.jpg",
+		"img/IU9b16_1/011.jpg",
+	];
+}
+
+function get_img_IU16b9_1()
+{
+	return [
+		"img/IU16b9_1/001.jpg",
+		"img/IU16b9_1/002.jpg",
+		"img/IU16b9_1/003.jpg",
+		"img/IU16b9_1/004.jpg",
+		"img/IU16b9_1/005.jpg",
+		"img/IU16b9_1/006.jpg",
+		"img/IU16b9_1/007.jpg",
+		"img/IU16b9_1/008.jpg",
+		"img/IU16b9_1/009.jpg",
+	];
+}
